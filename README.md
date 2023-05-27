154927e0c70de5f57baaa16f3b8e2f1f7a377cfc
Aditya Gupta <adityagupta78609@gmail.com>
2023-05-27 16:00:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nitpOld-clone-
---
+++ nitp.js
@@ -0,0 +1,5 @@
+// for(let i=0;i<10;i++){
+// console.log("welcome to NIT Patna ");
+// console.log("welcome to NIT Patna 2 ");
+// }
+// window.alert("welcomes you to NIT Patna");
